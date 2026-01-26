a158c14b7ba5ed8aefafdd7b17aa81905d73720e
Adrian Ratiu <adrian.ratiu@collabora.com>
2026-01-26 20:02:38 +0200

    hook: introduce extensions.hookStdoutToStderr
    All hooks already redirect stdout to stderr with the exception of
    pre-push which has a known user who depends on the separate stdout
    versus stderr outputs (the git-lfs project).
    
    The pre-push behavior was a surprise which we found out about after
    causing a regression for git-lfs. Notably, it might not be the only
    exception (it's the one we know about). There might be more.
    
    This presents a challenge because stdout_to_stderr is required for
    hook parallelization, so run-command can buffer and de-interleave
    the hook outputs using ungroup=0, when hook.jobs > 1.
    
    Introduce an extension to enforce consistency: all hooks merge stdout
    into stderr and can be safely parallelized. This provides a clean
    separation and avoids breaking existing stdout vs stderr behavior.
    
    When this extension is disabled, the `hook.jobs` config has no
    effect for pre-push, to prevent garbled (interleaved) parallel
    output, so it runs sequentially like before.
    
    Alternatives I've considered to this extension include:
    1. Allowing pre-push to run in parallel with interleaved output.
    2. Always running pre-push sequentially (no parallel jobs for it).
    3. Making users (only git-lfs? maybe more?) fix their hooks to read
       stderr not stdout.
    
    Out of all these alternatives, I think this extension is the most
    reasonable compromise, to not break existing users, allow pre-push
    parallel jobs for those who need it (with correct outputs) and also
    future-proofing in case there are any more exceptions to be added.
    
    Signed-off-by: Adrian Ratiu <adrian.ratiu@collabora.com>

--- t/t1800-hook.sh
+++ t/t1800-hook.sh
@@ -398,6 +398,22 @@ test_expect_success 'client hooks: pre-push expects separate stdout and stderr' 
 	check_stdout_separate_from_stderr pre-push
 '
 
+test_expect_success 'client hooks: extension makes pre-push merge stdout to stderr' '
+	test_when_finished "rm -f stdout.actual stderr.actual" &&
+	git init --bare remote2 &&
+	git remote add origin2 remote2 &&
+	test_commit B &&
+	# repositoryformatversion=1 might be already set (eg default sha256)
+	# so check before using test_config to set it
+	{ test "$(git config core.repositoryformatversion)" = 1 ||
+	  test_config core.repositoryformatversion 1; } &&
+	git config set core.repositoryformatversion 1 &&
+	test_config extensions.hookStdoutToStderr true &&
+	setup_hooks pre-push &&
+	git push origin2 HEAD:main >stdout.actual 2>stderr.actual &&
+	check_stdout_merged_to_stderr pre-push
+'
+
 test_expect_success 'client hooks: commit hooks expect stdout redirected to stderr' '
 	hooks="pre-commit prepare-commit-msg \
 		commit-msg post-commit \
